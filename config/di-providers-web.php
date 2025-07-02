d0fd9ca857ded779c7df1ddf56fd73802a810250
Chris <chris@clyates.plus.com>
2025-07-02 14:08:13 +0100

    Remove di-providers-web.php

--- config/di-providers-web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use BeastBytes\Yii\Tracy\ContainerServiceProvider;
-
-return [
-    'beastbytes/yii-tracy/' . ContainerServiceProvider::class => ContainerServiceProvider::class,
-];
